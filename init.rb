48855d52473a29fb0cad2a849290947fe831f0da
Michael Bleigh <michael@intridea.com>
2008-04-26 16:31:54 -0400

    Added Andrew Ng's patch to require everything on startup. Should finally fix the dashed-instead-of-underscored-directory-name problem. [#2 status:resolved]

--- init.rb
+++ init.rb
@@ -1,3 +1,6 @@
 require 'active_record/acts/taggable_on'
+require 'tag'
+require 'tag_list'
+require 'tags_helper'
 # Include hook code here
 ActiveRecord::Base.send :include, ActiveRecord::Acts::TaggableOn
