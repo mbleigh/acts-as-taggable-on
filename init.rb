ea67b5370f921df06bb5a2953c0862ec0baf0125
Michael Bleigh <michael@intridea.com>
2008-05-03 09:29:59 -0400

    Added 'tagging' to require list in init.rb. [#4 status:resolved]

--- init.rb
+++ init.rb
@@ -2,5 +2,6 @@ require 'active_record/acts/taggable_on'
 require 'tag'
 require 'tag_list'
 require 'tags_helper'
+require 'tagging'
 # Include hook code here
 ActiveRecord::Base.send :include, ActiveRecord::Acts::TaggableOn
