65cd7ff3cedad17970f3c56a7312641b1acf2b6b
Trevor Smith <tsmith@alliancehealth.com>
2009-07-06 15:43:43 +0800

    Getting tag_counts with the :limit and :order options now works correctly.
    Signed-off-by: Calvin Yu <csyu77@gmail.com>

--- lib/acts_as_taggable_on/acts_as_taggable_on.rb
+++ lib/acts_as_taggable_on/acts_as_taggable_on.rb
@@ -204,7 +204,9 @@ module ActiveRecord
           { :select     => "#{Tag.table_name}.id, #{Tag.table_name}.name, COUNT(*) AS count", 
             :joins      => joins.join(" "),
             :conditions => conditions,
-            :group      => group_by
+            :group      => group_by,
+            :limit      => options[:limit],
+            :order      => options[:order]
           }
         end    
         
