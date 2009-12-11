bf66000ad74ea6258f57523dc06c2d885f46bc37
Tom-Eric <ik@tom-eric.info>
2009-12-11 11:10:57 +0100

    Spec for previous fix

--- spec/acts_as_taggable_on/tagging_spec.rb
+++ spec/acts_as_taggable_on/tagging_spec.rb
@@ -4,4 +4,13 @@ describe Tagging do
   before(:each) do
     @tagging = Tagging.new
   end
+  
+  it "should not be valid with a invalid tag" do
+    @tagging.taggable = TaggableModel.create(:name => "Bob Jones")
+    @tagging.tag = Tag.new(:name => "")
+    @tagging.context = "tags"
+
+    @tagging.should_not be_valid
+    @tagging.errors.on(:tag_id).should == "can't be blank"
+  end
 end
