0bd130bb8463256ee85ab3123e182bf18bc18ad6
Tom-Eric <ik@tom-eric.info>
2009-12-11 12:06:46 +0100

    Include TagHelper in the Rails init.

--- rails/init.rb
+++ rails/init.rb
@@ -2,5 +2,6 @@ require 'acts-as-taggable-on'
 
 ActiveRecord::Base.send :include, ActiveRecord::Acts::TaggableOn
 ActiveRecord::Base.send :include, ActiveRecord::Acts::Tagger
+ActionView::Base.send :include, TagsHelper if defined?(ActionView::Base)
 
 RAILS_DEFAULT_LOGGER.info "** acts_as_taggable_on: initialized properly."
