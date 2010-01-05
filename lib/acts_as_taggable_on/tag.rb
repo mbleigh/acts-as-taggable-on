f3bf4e743722f191b7c08090bfc49f8b3513e1b0
Tom-Eric <ik@tom-eric.info>
2010-01-05 20:42:21 +0100

    Added attr_accessible so acts-as-taggable-on can play nice with http://github.com/ryanb/trusted-params

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -1,12 +1,23 @@
 class Tag < ActiveRecord::Base
+  
+  attr_accessible :name
+  
+  ### ASSOCIATIONS:
+  
   has_many :taggings, :dependent => :destroy
+  
+  ### VALIDATIONS:
   
   validates_presence_of :name
   validates_uniqueness_of :name
   
+  ### NAMED SCOPES:
+  
   named_scope :named, lambda { |name| { :conditions => ["name = ?", name] } }
   named_scope :named_like, lambda { |name| { :conditions => ["name LIKE ?", "%#{name}%"] } }
   named_scope :named_like_any, lambda { |list| { :conditions => list.map { |tag| sanitize_sql(["name LIKE ?", tag.to_s]) }.join(" OR ") } }
+  
+  ### METHODS:
   
   # LIKE is used for cross-database case-insensitivity
   def self.find_or_create_with_like_by_name(name)
--- lib/acts_as_taggable_on/tagging.rb
+++ lib/acts_as_taggable_on/tagging.rb
@@ -1,4 +1,8 @@
 class Tagging < ActiveRecord::Base #:nodoc:
+  attr_accessible :tag, :tag_id, :context,
+                  :taggable, :taggable_type, :taggable_id,
+                  :tagger, :tagger_type, :tagger_id
+
   belongs_to :tag
   belongs_to :taggable, :polymorphic => true
   belongs_to :tagger, :polymorphic => true
