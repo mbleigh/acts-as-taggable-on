6066084f486071bf31667edd215e874cf2772baf
Szymon Nowak <szimek@gmail.com>
2010-03-02 14:54:49 +0100

    Use Bundler.

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source :gemcutter
+
+gem 'rails', :git => 'git://github.com/rails/rails.git'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+group :test do
+  gem 'rspec', :require => 'spec'
+end
+
--- lib/acts-as-taggable-on.rb
+++ lib/acts-as-taggable-on.rb
@@ -1,3 +1,17 @@
+begin
+  # Try to require the preresolved locked set of gems.
+  require File.expand_path('../.bundle/environment', __FILE__)
+rescue LoadError
+  # Fall back on doing an unlocked resolve at runtime.
+  require "rubygems"
+  require "bundler"
+  Bundler.setup
+end
+
+Bundler.require
+
+require 'active_record'
+
 require "acts_as_taggable_on/group_helper"
 require "acts_as_taggable_on/acts_as_taggable_on"
 require "acts_as_taggable_on/acts_as_tagger"
