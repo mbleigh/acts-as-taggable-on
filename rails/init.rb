80dd4429483481c3b4f572c02738eb7d48ef6e4e
Szymon Nowak <szimek@gmail.com>
2010-03-03 14:35:50 +0100

    Remove no longer used init.rb files.

--- rails/init.rb
+++
@@ -1,6 +0,0 @@
-require "acts-as-taggable-on"
-
-ActiveRecord::Base.send :include, ActiveRecord::Acts::TaggableOn
-ActiveRecord::Base.send :include, ActiveRecord::Acts::Tagger
-
-ActionView::Base.send :include, TagsHelper if defined?(ActionView::Base)
