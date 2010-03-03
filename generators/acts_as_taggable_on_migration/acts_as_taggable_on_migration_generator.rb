e0eda5568fefddab3645925eabb2dbc6793e1076
Szymon Nowak <szimek@gmail.com>
2010-03-03 10:31:17 +0100

    Updated generators to work with Rails 3. Regenerated gemspec file.

--- generators/acts_as_taggable_on_migration/acts_as_taggable_on_migration_generator.rb
+++
@@ -1,7 +0,0 @@
-class ActsAsTaggableOnMigrationGenerator < Rails::Generator::Base 
-  def manifest 
-    record do |m| 
-      m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "acts_as_taggable_on_migration"
-    end
-  end
-end
--- lib/generators/acts_as_taggable_on/migration/templates/active_record/migration.rb
+++ lib/generators/acts_as_taggable_on/migration/templates/active_record/migration.rb
@@ -1,27 +1,26 @@
 class ActsAsTaggableOnMigration < ActiveRecord::Migration
   def self.up
     create_table :tags do |t|
-      t.column :name, :string
+      t.string :name
     end
-    
+
     create_table :taggings do |t|
-      t.column :tag_id, :integer
-      t.column :taggable_id, :integer
-      t.column :tagger_id, :integer
-      t.column :tagger_type, :string
-      
+      t.references :tag
+
       # You should make sure that the column created is
       # long enough to store the required class names.
-      t.column :taggable_type, :string
-      t.column :context, :string
-      
-      t.column :created_at, :datetime
+      t.references :taggable, :polymorphic => true
+      t.references :tagger, :polymorphic => true
+
+      t.string :context
+
+      t.datetime :created_at
     end
-    
+
     add_index :taggings, :tag_id
     add_index :taggings, [:taggable_id, :taggable_type, :context]
   end
-  
+
   def self.down
     drop_table :taggings
     drop_table :tags
