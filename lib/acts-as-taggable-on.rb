063e1632d2c4caf799e3dc226b2d3ad5e44dacc6
Szymon Nowak <szimek@gmail.com>
2010-03-03 14:35:50 +0100

    Remove no longer used init.rb files.

--- lib/acts-as-taggable-on.rb
+++ lib/acts-as-taggable-on.rb
@@ -1,6 +1,6 @@
 begin
   # Try to require the preresolved locked set of gems.
-  require File.expand_path('../.bundle/environment', __FILE__)
+  require File.expand_path("../.bundle/environment", __FILE__)
 rescue LoadError
   # Fall back on doing an unlocked resolve at runtime.
   require "rubygems"
@@ -10,7 +10,8 @@ end
 
 Bundler.require
 
-require 'active_record'
+require "active_record"
+require "action_view"
 
 require "acts_as_taggable_on/group_helper"
 require "acts_as_taggable_on/acts_as_taggable_on"
@@ -19,3 +20,8 @@ require "acts_as_taggable_on/tag"
 require "acts_as_taggable_on/tag_list"
 require "acts_as_taggable_on/tags_helper"
 require "acts_as_taggable_on/tagging"
+
+ActiveRecord::Base.send :include, ActiveRecord::Acts::TaggableOn
+ActiveRecord::Base.send :include, ActiveRecord::Acts::Tagger
+
+ActionView::Base.send :include, TagsHelper
