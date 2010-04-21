976cab3b8b80b0a249135df2f5d6ef87cd4be390
Gus Ortiz <guspidey@gmail.com>
2010-04-22 02:36:47 +0800

    Fixed migration_generation Join bug

--- lib/generators/acts_as_taggable_on/migration/migration_generator.rb
+++ lib/generators/acts_as_taggable_on/migration/migration_generator.rb
@@ -11,7 +11,7 @@ module ActsAsTaggableOn
     end
 
     def self.source_root
-      File.join(File.dirname(__FILE__), 'templates', orm)
+      File.join(File.dirname(__FILE__), 'templates', (orm.to_s unless orm.class.eql?(String)) )
     end
 
     def self.orm_has_migration?
@@ -29,3 +29,4 @@ module ActsAsTaggableOn
     end
   end
 end
+
