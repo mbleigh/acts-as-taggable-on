21556713980f96ab7978fdd7687e06c7b333e1fa
Tom-Eric <ik@tom-eric.info>
2010-05-18 10:11:38 +0200

    Start of multi db test suite. This will help with PostgreSQL bugs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,21 +29,24 @@ unless [].respond_to?(:freq)
   end
 end
 
-# Setup a database
-TEST_DATABASE_FILE = File.join(File.dirname(__FILE__), '..', 'test.sqlite3')
-File.unlink(TEST_DATABASE_FILE) if File.exist?(TEST_DATABASE_FILE)
+ENV['DB'] ||= 'sqlite3'
 
-ActiveRecord::Base.establish_connection(
-  "adapter" => "sqlite3", "database" => TEST_DATABASE_FILE
-)
-
-ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
-ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => TEST_DATABASE_FILE
-
-ActiveRecord::Base.silence do
-  ActiveRecord::Migration.verbose = false
-  load(File.dirname(__FILE__) + '/schema.rb')
-  load(File.dirname(__FILE__) + '/models.rb')
+database_yml = File.expand_path('../database.yml', __FILE__)
+if File.exists?(database_yml)
+  active_record_configuration = YAML.load_file(database_yml)[ENV['DB']]
+  
+  ActiveRecord::Base.establish_connection(active_record_configuration)
+  ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
+  
+  ActiveRecord::Base.silence do
+    ActiveRecord::Migration.verbose = false
+    
+    load(File.dirname(__FILE__) + '/schema.rb')
+    load(File.dirname(__FILE__) + '/models.rb')
+  end  
+  
+else
+  raise "Please create #{database_yml} first to configure your database. Take a look at: #{database_yml}.sample"
 end
 
 def clean_database!
