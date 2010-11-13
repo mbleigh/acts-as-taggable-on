67f90141c206e2750cf1cbed702323c1d1ca1004
David N. Welton <davidw@dedasys.com>
2010-11-13 22:47:40 +0100

    Actually report which gem wasn't installed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,9 +11,8 @@ begin
 
   # Set up load paths for all bundled gems
   Bundler.setup
-rescue Bundler::GemNotFound
-  raise RuntimeError, "Bundler couldn't find some gems." +
-    "Did you run `bundle install`?"
+rescue Bundler::GemNotFound => e
+  raise RuntimeError, "Bundler couldn't find some gems: (#{e}) Did you run `bundle install`?"
 end
 
 Bundler.require
