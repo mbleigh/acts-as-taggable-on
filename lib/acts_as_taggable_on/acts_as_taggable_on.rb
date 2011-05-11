04e52a54b43285e4bf2360aaaab1c51e0909265c
Mike Lapshin <sotakone@sotakone.com>
2011-05-11 21:07:36 +0400

    class_inheritable_attribute replaced with class_attribute (rails 3.1 compatibility)

--- lib/acts_as_taggable_on/acts_as_taggable_on.rb
+++ lib/acts_as_taggable_on/acts_as_taggable_on.rb
@@ -26,13 +26,13 @@ module ActsAsTaggableOn
     #   end
     def acts_as_taggable_on(*tag_types)
       tag_types = tag_types.to_a.flatten.compact.map(&:to_sym)
+      class_attribute :tag_types
 
       if taggable?
-        write_inheritable_attribute(:tag_types, (self.tag_types + tag_types).uniq)
+        self.tag_types = (self.tag_types + tag_types).uniq
       else
-        write_inheritable_attribute(:tag_types, tag_types)
-        class_inheritable_reader(:tag_types)
-        
+        self.tag_types = tag_types
+
         class_eval do
           has_many :taggings, :as => :taggable, :dependent => :destroy, :include => :tag, :class_name => "ActsAsTaggableOn::Tagging"
           has_many :base_tags, :through => :taggings, :source => :tag, :class_name => "ActsAsTaggableOn::Tag"
@@ -40,7 +40,7 @@ module ActsAsTaggableOn
           def self.taggable?
             true
           end
-        
+
           include ActsAsTaggableOn::Taggable::Core
           include ActsAsTaggableOn::Taggable::Collection
           include ActsAsTaggableOn::Taggable::Cache
