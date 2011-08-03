72c8cd1f5a653bdfc817a5eec1ed8ee7ea2c6886
artemk <kramarenko.artyom@gmail.com>
2011-08-03 22:22:33 +0300

    change ci script

--- spec/ci.sh
+++ spec/ci.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-cp spec/database.yml.example spec/database.yml && bundle install && bundle exec rake prepare_ci_env db:create db:migrate && bundle exec rake spec
+cp spec/database.yml.example spec/database.yml && bundle install && bundle exec rake
