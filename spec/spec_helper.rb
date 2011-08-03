20599f0801c2921f5a09c9d00c0e17fb8003176c
artemk <kramarenko.artyom@gmail.com>
2011-08-04 00:17:21 +0300

    create databases for specs if dbs are missed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,8 +37,24 @@ if File.exists?(database_yml)
   active_record_configuration = YAML.load_file(database_yml)
   
   ActiveRecord::Base.configurations = active_record_configuration
+  config = ActiveRecord::Base.configurations[db_name]
   
-  ActiveRecord::Base.establish_connection(db_name)
+  begin
+    ActiveRecord::Base.establish_connection(db_name)
+    ActiveRecord::Base.connection
+  rescue
+    case db_name
+    when /mysql/      
+      ActiveRecord::Base.establish_connection(config.merge('database' => nil))
+      ActiveRecord::Base.connection.create_database(config['database'],  {:charset => 'utf8', :collation => 'utf8_unicode_ci'})
+    when 'postgresql'
+      ActiveRecord::Base.establish_connection(config.merge('database' => 'postgres', 'schema_search_path' => 'public'))
+      ActiveRecord::Base.connection.create_database(config['database'], config.merge('encoding' => 'utf8'))
+    end
+    
+    ActiveRecord::Base.establish_connection(config)
+  end
+    
   ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
   ActiveRecord::Base.default_timezone = :utc
   
