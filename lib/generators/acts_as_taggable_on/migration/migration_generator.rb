b86986a00f53312648edefdee36680a66d211687
artemk <kramarenko.artyom@gmail.com>
2011-08-22 10:27:50 +0300

    fixes for migration number #181

--- lib/generators/acts_as_taggable_on/migration/migration_generator.rb
+++ lib/generators/acts_as_taggable_on/migration/migration_generator.rb
@@ -1,3 +1,4 @@
+require 'rails/generators'
 require 'rails/generators/migration'
 
 module ActsAsTaggableOn
@@ -18,8 +19,14 @@ module ActsAsTaggableOn
       [:active_record].include? orm
     end
 
-    def self.next_migration_number(path)
-      ActiveRecord::Generators::Base.next_migration_number(path)
+    def self.next_migration_number(dirname)
+      if ActiveRecord::Base.timestamped_migrations
+        migration_number = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        migration_number += 1
+        migration_number.to_s
+      else
+        "%.3d" % (current_migration_number(dirname) + 1)
+      end
     end
 
     def create_migration_file
