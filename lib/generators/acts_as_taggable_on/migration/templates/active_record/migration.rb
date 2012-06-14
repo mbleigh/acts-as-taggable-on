df2260ef5c57d66f08a6da587d5041229fa247e0
Brian Gesiak <modocache@gmail.com>
2012-06-14 14:44:38 -0300

    Fix comment typo in migration template.

--- lib/generators/acts_as_taggable_on/migration/templates/active_record/migration.rb
+++ lib/generators/acts_as_taggable_on/migration/templates/active_record/migration.rb
@@ -12,8 +12,8 @@ class ActsAsTaggableOnMigration < ActiveRecord::Migration
       t.references :taggable, :polymorphic => true
       t.references :tagger, :polymorphic => true
 
-      # limit is created to prevent mysql error o index lenght for myisam table type.
-      # http://bit.ly/vgW2Ql
+      # Limit is created to prevent MySQL error on index
+      # length for MyISAM table type: http://bit.ly/vgW2Ql
       t.string :context, :limit => 128
 
       t.datetime :created_at
