15ee3933e922c0801241b05fd0717a5a2f753530
Jonathan del Strother <jon.delStrother@audioboo.fm>
2012-10-11 11:32:02 +0100

    Allow subclasses to override the tag_list methods

--- spec/models.rb
+++ spec/models.rb
@@ -4,6 +4,12 @@ class TaggableModel < ActiveRecord::Base
   acts_as_taggable_on :skills
   acts_as_taggable_on :needs, :offerings
   has_many :untaggable_models
+
+  attr_reader :tag_list_submethod_called
+  def tag_list=v
+    @tag_list_submethod_called = true
+    super
+  end
 end
 
 class CachedModel < ActiveRecord::Base
