2817b35881752adcc2004538393c3247478d2336
Benjamin Fleischer <github@benjaminfleischer.com>
2013-12-27 10:44:10 -0600

    Adds acts-as-taggable-on to gem path for backwards compatibility
    Outputs a warning to $stderr

---
+++ lib/acts-as-taggable-on.rb
@@ -0,0 +1,7 @@
+$stderr.puts <<EOF
+You are requiring 'acts-as-taggable-on' in the 'acts_as_taggable_on' gem.
+This gem is a fork where new versions of the library are published.
+
+Please change your require to 'acts_as_taggable_on'.
+EOF
+require_relative 'acts_as_taggable_on'
