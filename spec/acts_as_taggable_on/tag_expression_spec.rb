4ff82a2eeb58e7b72b53baf032dc96985453b6e6
Jake Hurley <jh2706@nyu.edu>
2014-01-04 08:44:44 -0500

    updated tests for new options format

--- spec/acts_as_taggable_on/tag_expression_spec.rb
+++ spec/acts_as_taggable_on/tag_expression_spec.rb
@@ -20,8 +20,8 @@ describe "Tag Expressions" do
     TaggableModel.create(:name => "Bob", :tag_list => "c++, java, ruby")
     TaggableModel.create(:name => 'Steve', :tag_list => "ruby, python")
 
-    TaggableModel.tagged_with("c++ + python", :expression => true, :use_whitespace => true).to_a.length.should == 2
-    TaggableModel.tagged_with("c++ ++++++++++ python", :expression => true, :use_whitespace => true).to_a.length.should == 2
+    TaggableModel.tagged_with("c++ + python", :expression => { :use_whitespace => true }).to_a.length.should == 2
+    TaggableModel.tagged_with("c++ ++++++++++ python", :expression => { :use_whitespace => true }).to_a.length.should == 2
 
   end
 
@@ -42,8 +42,8 @@ describe "Tag Expressions" do
     TaggableModel.create(:name => "Bob", :tag_list => "c++, java, ruby")
     TaggableModel.create(:name => 'Steve', :tag_list => "ruby, python")
 
-    TaggableModel.tagged_with("c\\+\\+++++python", :expression => true, :use_whitespace => false).to_a.length.should == 2
-    TaggableModel.tagged_with("c++ ++++++++++ python", :expression => true, :use_whitespace => true).to_a.length.should == 2
+    TaggableModel.tagged_with("c\\+\\+++++python", :expression => { :use_whitespace => false }).to_a.length.should == 2
+    TaggableModel.tagged_with("c++ ++++++++++ python", :expression => {:use_whitespace => true }).to_a.length.should == 2
   end
 
   it "should permit operators preceded by a backward slash in tag name" do
@@ -76,16 +76,16 @@ describe "Tag Expressions" do
     chain.should == expression
   end
 
-  it "should produce better results than ActiveRecord chaining in more complex cases" do
-    TaggableModel.create(:name => "Bob", :tag_list => "sad, lazy, strong")
-    TaggableModel.create(:name => "Frank", :tag_list => "happy, strong")
-    TaggableModel.create(:name => 'Steve', :tag_list => "happy, lazy")
-    TaggableModel.create(:name => 'Steve+Molly', :tag_list => "sad, strong, lazy, grumpy")
+  it "should allow union to act as an intersection using default_chaining option" do
+    bob = TaggableModel.create(:name => "Bob", :tag_list => "sad, lazy, happy, grumpy")
+    TaggableModel.create(:name => "Frank", :tag_list => "happy, lazy, strong")
+    TaggableModel.create(:name => 'Steve', :tag_list => "happy, grumpy, lazy")
+    TaggableModel.create(:name => 'Steve+Molly', :tag_list => "sad, strong, lazy, happy")
 
-    default_chain = TaggableModel.tagged_with("sad&lazy+happy", :expression => true, :default_chaining => true).to_a
-    fixed_chain = TaggableModel.tagged_with("sad&lazy+happy", :expression => true, :default_chaining => false).to_a
+    default_chain = TaggableModel.tagged_with("sad+lazy+happy+grumpy", :expression => {:default_chaining => true}).to_a
+    intersection_chain = TaggableModel.tagged_with("sad&lazy&happy&grumpy", :expression => {:default_chaining => false}).to_a
 
-    default_chain.length.should == 0
-    fixed_chain.length.should == 3
+    default_chain.should == [bob]
+    intersection_chain.should == [bob]
   end
 end
