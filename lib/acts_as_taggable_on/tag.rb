ccfe7b2d9cfa91087fdafd5d18e4c829a4255e65
arni1981 <arni1981@gmail.com>
2014-01-30 09:25:23 +0000

    Update tag.rb
    Added support for international characters.

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -26,7 +26,7 @@ module ActsAsTaggableOn
       if ActsAsTaggableOn.strict_case_match
         where(["name = #{binary}?", name])
       else
-        where(["lower(name) = ?", name.downcase])
+        where(["lower(name) = ?", as_8bit_ascii(name).downcase])
       end
     end
 
