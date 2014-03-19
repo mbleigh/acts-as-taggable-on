603c55be3901597a958f98be875cefd62d58c00a
Felipe Lopes <felipelopes10@gmail.com>
2014-03-19 08:09:24 -0300

    Adding test to check tags outside date range

--- spec/acts_as_taggable_on/taggable_spec.rb
+++ spec/acts_as_taggable_on/taggable_spec.rb
@@ -224,11 +224,18 @@ describe "Taggable" do
     expect(TaggableModel.tagged_with("ruby").group(:created_at).count.count).to eq(1)
   end
 
-  it "should be able to get find a tag using dates" do
+  it "should be able to find a tag using dates" do
     @taggable.skill_list = "ruby"
     @taggable.save
 
     expect(TaggableModel.tagged_with("ruby", :start_at => Date.today, :end_at => Date.tomorrow).count).to eq(1)
+  end
+
+    it "shouldn't be able to find a tag outside date range" do
+    @taggable.skill_list = "ruby"
+    @taggable.save
+
+    expect(TaggableModel.tagged_with("ruby", :start_at => Date.today - 2.days, :end_at => Date.today - 1.day).count).to eq(0)
   end
 
   it "should be able to find by tag with context" do
