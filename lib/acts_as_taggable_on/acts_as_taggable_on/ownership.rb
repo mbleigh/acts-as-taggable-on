bf3964300deeed1a513be9906bb1e27c18d0507e
Abdelkader Boudih <terminale@gmail.com>
2014-04-16 16:15:35 +0000

    Delete unused owned tags directly.

--- lib/acts_as_taggable_on/acts_as_taggable_on/ownership.rb
+++ lib/acts_as_taggable_on/acts_as_taggable_on/ownership.rb
@@ -108,16 +108,9 @@ module ActsAsTaggableOn::Taggable
 
           # Find all taggings that belong to the taggable (self), are owned by the owner,
           # have the correct context, and are removed from the list.
-          if old_tags.present?
-            old_taggings = ActsAsTaggableOn::Tagging.where(:taggable_id => id, :taggable_type => self.class.base_class.to_s,
+          ActsAsTaggableOn::Tagging.destroy_all(:taggable_id => id, :taggable_type => self.class.base_class.to_s,
                                                             :tagger_type => owner.class.base_class.to_s, :tagger_id => owner.id,
-                                                            :tag_id => old_tags, :context => context)
-          end
-
-          # Destroy old taggings:
-          if old_taggings.present?
-            ActsAsTaggableOn::Tagging.destroy_all(:id => old_taggings.map(&:id))
-          end
+                                                            :tag_id => old_tags, :context => context) if old_tags.present?
 
           # Create new taggings:
           new_tags.each do |tag|
