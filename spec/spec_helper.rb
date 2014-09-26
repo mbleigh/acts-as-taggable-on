60a2794d37fceb64841a6734ed1ab8825dc3f5df
Jake Mitchell <jakemitchell@jake3p0.com>
2014-09-25 22:52:02 -0400

    Add test helper to view current contents of regular and namespaced tables

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,4 +19,3 @@ Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
 RSpec.configure do |config|
   config.raise_errors_for_deprecations!
 end
-
--- spec/support/0-helpers.rb
+++ spec/support/0-helpers.rb
@@ -30,3 +30,35 @@ end
 def using_case_insensitive_collation?
   using_mysql? && ActsAsTaggableOn::Utils.connection.collation =~ /_ci\Z/
 end
+
+# Quickly retrieve table contents for debugging...feel free
+# to modify
+def look_at_database(only: nil)
+  puts "\n\n========== Database Tables =========="
+  if only.nil? or only == :tags
+    puts "\n---tags---"
+    puts ActiveRecord::Base.connection.execute("SELECT * FROM tags").inspect
+  end
+  if only.nil? or only == :taggings
+    puts "\n---taggings---"
+    puts ActiveRecord::Base.connection.execute("SELECT * FROM taggings").inspect
+  end
+  if only.nil? or only == :taggable_models
+    puts "\n---taggable_models---"
+    puts ActiveRecord::Base.connection.execute("SELECT * FROM taggable_models").inspect
+  end
+  puts "\n\n---\n\n" if only.nil?
+  if only.nil? or only == :tags
+    puts "\n---nspaced_tags---"
+    puts ActiveRecord::Base.connection.execute("SELECT * FROM nspaced_tags").inspect
+  end
+  if only.nil? or only == :taggigns
+    puts "\n---nspaced_taggings---"
+    puts ActiveRecord::Base.connection.execute("SELECT * FROM nspaced_taggings").inspect
+  end
+  if only.nil? or only == :taggable_models
+    puts "\n---taggable_namespaced_models---"
+    puts ActiveRecord::Base.connection.execute("SELECT * FROM taggable_namespaced_models").inspect
+  end
+  puts "\n================ End ================\n\n"
+end
