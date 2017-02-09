dcbae3a82cdfa9617998ba6ca1297183c30d30ae
“Pavel <pavel.jurasek@bcgdv.com>
2017-02-09 08:02:04 +0100

    Replace id to uuid and inherit from ApplicationRecord

--- db/migrate/1_acts_as_taggable_on_migration.rb
+++ db/migrate/1_acts_as_taggable_on_migration.rb
@@ -1,10 +1,10 @@
 class ActsAsTaggableOnMigration < ActiveRecord::Migration
   def self.up
-    create_table :tags do |t|
+    create_table :tags, id: :uuid do |t|
       t.string :name
     end
 
-    create_table :taggings do |t|
+    create_table :taggings, id: :uuid do |t|
       t.references :tag
 
       # You should make sure that the column created is
