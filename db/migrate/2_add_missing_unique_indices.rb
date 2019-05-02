98bf9b6aa8bcfaa3ee0b662b8d652942a2b1f3eb
Damian Legawiec <damian@sparksolutions.co>
2019-05-02 16:02:20 +0200

    Fixed MySQL migration in AR 6.0

--- db/migrate/2_add_missing_unique_indices.rb
+++ db/migrate/2_add_missing_unique_indices.rb
@@ -7,7 +7,12 @@ AddMissingUniqueIndices.class_eval do
   def self.up
     add_index ActsAsTaggableOn.tags_table, :name, unique: true
 
-    remove_index ActsAsTaggableOn.taggings_table, :tag_id if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
+    if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
+      if foreign_key_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
+        remove_foreign_key ActsAsTaggableOn.taggings_table, column: :tag_id
+      end
+      remove_index ActsAsTaggableOn.taggings_table, :tag_id
+    end
     remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx'
     add_index ActsAsTaggableOn.taggings_table,
               [:tag_id, :taggable_id, :taggable_type, :context, :tagger_id, :tagger_type],
