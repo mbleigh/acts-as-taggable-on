ad1166028745e5e27690b845b60ec777f9d5fc01
Marc Jeffrey <mjeffrey18@gmail.com>
2019-10-07 22:51:02 +0400

    added quickfix for rails 6 upgrade

--- lib/acts_as_taggable_on/taggable/core.rb
+++ lib/acts_as_taggable_on/taggable/core.rb
@@ -50,7 +50,7 @@ module ActsAsTaggableOn::Taggable
               parsed_new_list = ActsAsTaggableOn.default_parser.new(new_tags).parse
 
               if self.class.preserve_tag_order? || parsed_new_list.sort != #{tag_type}_list.sort
-                set_attribute_was('#{tag_type}_list', #{tag_type}_list)
+                duplicated_mutations_from_database.change_to_attribute('#{tag_type}_list')
                 write_attribute("#{tag_type}_list", parsed_new_list)
               end
 
