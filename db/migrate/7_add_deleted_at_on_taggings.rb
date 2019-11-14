47c95673573b98bba28f68d6ea0a3ffd05803bca
Rolando Alvarado <cartechcebu@gmail.com>
2019-11-14 10:50:07 +0800

    removed commented code

--- db/migrate/7_add_deleted_at_on_taggings.rb
+++ db/migrate/7_add_deleted_at_on_taggings.rb
@@ -1,19 +1,4 @@
 if ActiveRecord.gem_version >= Gem::Version.new('5.0')
-#<<<<<<< HEAD
-#   class AddMissingIndexesOnTaggings < ActiveRecord::Migration[4.2]; end
-# else
-#   class AddMissingIndexesOnTaggings < ActiveRecord::Migration; end
-# end
-# AddDeletedAtOnTaggings.class_eval do
-#   def self.up
-#     add_column ActsAsTaggableOn.taggings_table, :deleted_at, :datetime
-#     add_index ActsAsTaggableOn.taggings_table, :deleted_at unless index_exists? ActsAsTaggableOn.taggings_table, :deleted_at
-#   end
-
-#   def self.down
-#     index_exists? ActsAsTaggableOn.taggings_table, :deleted_at && remove_index ActsAsTaggableOn.taggings_table, :deleted_at
-#     column_exists? ActsAsTaggableOn.taggings_table, :deleted_at && remove_column ActsAsTaggableOn.taggings_table, :deleted_at
-#=======
   class AddDeletedAtOnTaggings < ActiveRecord::Migration[4.2]; end
 else
   class AddDeletedAtOnTaggings < ActiveRecord::Migration; end
@@ -28,5 +13,4 @@ AddDeletedAtOnTaggings.class_eval do
     index_exists?(ActsAsTaggableOn.taggings_table, :deleted_at) && remove_index(ActsAsTaggableOn.taggings_table, :deleted_at)
     column_exists?(ActsAsTaggableOn.taggings_table, :deleted_at) && remove_column(ActsAsTaggableOn.taggings_table, :deleted_at)
   end
-#>>>>>>> Added ignore_deleted option
 end
