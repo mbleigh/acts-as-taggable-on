e44be1cb53c13483c4595b34a7cb79570d2b45c5
Abdelkader Boudih <terminale@gmail.com>
2021-11-09 14:49:54 +0100

    chore: Add docker-compose.yml

--- spec/support/database.rb
+++ spec/support/database.rb
@@ -5,12 +5,17 @@ database_yml = File.expand_path('../../internal/config/database.yml', __FILE__)
 
 if File.exist?(database_yml)
 
-  ActiveRecord::Migration.verbose = false
-  ActiveRecord::Base.default_timezone = :utc
   ActiveRecord::Base.configurations = YAML.load_file(database_yml)
   ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), '../debug.log'))
   ActiveRecord::Base.logger.level = ENV['TRAVIS'] ? ::Logger::ERROR : ::Logger::DEBUG
-  config = ActiveSupport::HashWithIndifferentAccess.new(ActiveRecord::Base.configurations[db_name])
+  ActiveRecord::Migration.verbose = false
+  if ActiveRecord.version >= Gem::Version.new('7.0.0.alpha2')
+    ActiveRecord.default_timezone = :utc
+    config = ActiveRecord::Base.configurations.configs_for(env_name: db_name)
+  else
+    ActiveRecord::Base.default_timezone = :utc
+    config = ActiveSupport::HashWithIndifferentAccess.new(ActiveRecord::Base.configurations[db_name])
+  end
 
   begin
     ActiveRecord::Base.establish_connection(db_name.to_sym)
